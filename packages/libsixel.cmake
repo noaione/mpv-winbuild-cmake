84d9a05a782d5560dbc5fea62f0553337fd569b3
noaione <noaione0809@gmail.com>
2024-08-28 10:40:43 +0700

    libsixel: change repository and enable on mpv build

--- packages/libsixel.cmake
+++ packages/libsixel.cmake
@@ -2,23 +2,26 @@ ExternalProject_Add(libsixel
     DEPENDS
         libpng
         libjpeg
-    GIT_REPOSITORY https://github.com/saitoha/libsixel.git
+    GIT_REPOSITORY https://github.com/libsixel/libsixel.git
+    SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} CONF=1 <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
+    CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
-        --with-jpeg
-        --with-png
-        --disable-img2sixel
-        --disable-sixel2png
-        --disable-python
-        --enable-static
-        --disable-shared
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
+        --libdir=${MINGW_INSTALL_PREFIX}/lib
+        --cross-file=${MESON_CROSS}
+        --buildtype=release
+        --default-library=static
+        -Djpeg=enabled
+        -Dpng=enabled
+        -Dimg2sixel=disabled
+        -Dsixel2png=disabled
+        "-Dc_args='-Wno-implicit-function-declaration'"
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 force_rebuild_git(libsixel)
+force_meson_configure(libsixel)
 cleanup(libsixel install)
